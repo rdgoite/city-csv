7b90f4e6e8a32aef80285f7fbc55ea5caf96507a
rdgoite <rdgoite@gmail.com>
2016-05-29 00:35:00 +0800

    Created new project for downloading city data into CSV.

---
+++ src/main/java/de/goeuro/demo/CityCsvApplication.java
@@ -0,0 +1,12 @@
+package de.goeuro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityCsvApplication.class, args);
+	}
+}
---
+++ src/test/java/de/goeuro/demo/CityCsvApplicationTests.java
@@ -0,0 +1,16 @@
+package de.goeuro.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CityCsvApplication.class)
+public class CityCsvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
