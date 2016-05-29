34415e95600b910155272e735c8804f39b761715
rdgoite <rdgoite@gmail.com>
2016-05-29 21:12:51 +0800

    Defined components for running the application.

--- src/main/java/de/goeuro/demo/CityCsvApplication.java
+++ src/main/java/de/goeuro/demo/CityCsvApplication.java
@@ -2,11 +2,40 @@ package de.goeuro.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CityCsvApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CityCsvApplication.class, args);
-	}
+    @Configuration
+    protected static class ApplicationConfiguration {
+
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
+
+        @Bean
+        public LocationService locationService() {
+            return new LocationService();
+        }
+
+        @Bean
+        public CsvDownloader csvDownloader() {
+            return new CsvDownloader();
+        }
+
+        @Bean
+        public CsvDownloadRunner csvDownloadRunner() {
+            return new CsvDownloadRunner(csvDownloader());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityCsvApplication.class, args);
+    }
+
 }
