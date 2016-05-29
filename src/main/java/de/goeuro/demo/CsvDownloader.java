d248857053ceb3405dbedf3eb8d5dd44ba9bb5ba
rdgoite <rdgoite@gmail.com>
2016-05-29 13:46:01 +0800

    Added code for creating output file.

--- src/main/java/de/goeuro/demo/CsvDownloader.java
+++ src/main/java/de/goeuro/demo/CsvDownloader.java
@@ -1,5 +1,17 @@
 package de.goeuro.demo;
 
+import java.io.File;
+import java.io.IOException;
+
 public class CsvDownloader {
+
+    public void download(String keyword) {
+        File file = new File("location.csv");
+        try {
+            file.createNewFile();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 
 }
--- src/test/java/de/goeuro/demo/csvdownloader/CsvDownloaderTest.java
+++ src/test/java/de/goeuro/demo/csvdownloader/CsvDownloaderTest.java
@@ -1,19 +1,24 @@
 package de.goeuro.demo.csvdownloader;
 
 import de.goeuro.demo.CsvDownloader;
+import de.goeuro.demo.Location;
 import de.goeuro.demo.LocationService;
-import org.junit.Before;
-import org.junit.Rule;
+import de.goeuro.demo.PositionSuggestion;
+import org.junit.After;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
-import static org.junit.Assert.assertNotNull;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={CsvDownloaderConfiguration.class})
@@ -25,19 +30,39 @@ public class CsvDownloaderTest {
     @Autowired
     private CsvDownloader downloader;
 
-    @Rule
-    public TemporaryFolder outputFolder = new TemporaryFolder();
+    private Set<String> temporaryFiles = new HashSet<>();
 
-    @Before
-    public void setUp() throws Exception {
-        outputFolder.newFolder("output");
+    @After
+    public void cleanUp() {
+        for (String fileName : temporaryFiles) {
+            new File(fileName).delete();
+        }
+        temporaryFiles.clear();
     }
 
     @Test
-    public void testDownloadCsv() {
-        //expect:
-        assertNotNull(locationService);
-        assertNotNull(downloader);
+    public void testDownload() throws Exception {
+        //given:
+        Location berlin = mock(Location.class);
+        doReturn("123,Berlin,location,12.3,45.6").when(berlin).toCsv();
+
+        //and:
+        Location berlingerode = mock(Location.class);
+        doReturn("777,Berlingerode,location,98.7,65.4").when(berlingerode).toCsv();
+
+        //and:
+        String searchKey = "Berlin";
+        PositionSuggestion suggestion = new PositionSuggestion(asList(berlin, berlingerode));
+        doReturn(suggestion).when(locationService).suggestPosition(searchKey);
+
+        //when:
+        downloader.download(searchKey);
+
+        //then:
+        String expectedFileName = "location.csv";
+        File outputFile = new File(expectedFileName);
+        assertTrue(outputFile.exists());
+        temporaryFiles.add(expectedFileName);
     }
 
 }
