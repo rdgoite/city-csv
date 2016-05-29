cf5211ec15a55fe7779b49c86643a87406a36dbb
rdgoite <rdgoite@gmail.com>
2016-05-29 21:38:56 +0800

    Updated code so that default override mode can be set in properties file.

--- src/main/java/de/goeuro/demo/CsvDownloadRunner.java
+++ src/main/java/de/goeuro/demo/CsvDownloadRunner.java
@@ -1,5 +1,6 @@
 package de.goeuro.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
@@ -9,6 +10,9 @@ public class CsvDownloadRunner implements ApplicationRunner {
 
     public static final String MESSAGE_SUCCESS = "Download completed with no errors.";
     public static final String MESSAGE_KEYWORD_COUNT = "Expected exactly 1 keyword.";
+
+    @Value("${csv.download.override:false}")
+    private String defaultOverrideMode;
 
     private final CsvDownloader downloader;
 
@@ -21,7 +25,7 @@ public class CsvDownloadRunner implements ApplicationRunner {
         List<String> nonOptionArgs = args.getNonOptionArgs();
         if (nonOptionArgs.size() == 1) {
             String keyword = nonOptionArgs.get(0);
-            String override = "false";
+            String override = defaultOverrideMode;
             String forceOption = "force";
             if (args.containsOption(forceOption)) {
                 override = args.getOptionValues(forceOption).get(0);
